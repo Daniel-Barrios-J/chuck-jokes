d081d29572c08cd36af4b167199b8abf8b32dac4
Daniel Barrios J <daniel.bj.rd@gmail.com>
2022-10-22 15:10:17 -0500

    cambios estilos

--- src/pages/chuck_jokes/ChuckJokes.jsx
+++ src/pages/chuck_jokes/ChuckJokes.jsx
@@ -1,20 +1,23 @@
 import React, {useState} from 'react';
 // MUI 5
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import { Badge } from '@mui/material';
 import Typography from '@mui/material/Typography';
 //services
 import { getJoke } from '../../services/chuckJokes';
+//styles
+import '../../styles/css/chuck.css'
 
 const ChuckJokes = () => {
+
+  const descripcionInicial = 'Pide un chiste de Chuck Norris y luego calificalo con los botones debajo, NOTA: los chistes están en inglés. 👌';
 
   const [voteJokeState, setVoteJoke] = useState(false);
   const [duuh, setDuuh] = useState(0);
   const [haha, setHaha] = useState(0);
-  const [jokes, setJokes] = useState('');
-  
+  const [jokes, setJokes] = useState(descripcionInicial);
+
+
   const joke = ()=>{
     getJoke().then(r =>{
       setJokes(r.data.value)
@@ -35,27 +38,27 @@ const ChuckJokes = () => {
   }
 
    return (
-    <div>
-      <Card sx={{ minWidth: 275, maxWidth: 300 }} style={{}}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Chuck says
-          </Typography>
-          <img style={{width: '220px'}} alt='chuck-logo' src='https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png'/>
-          <Typography variant="body2" style={{minHeight: '50px', paddingTop: '16px'}}>
+    <div className='card'>
+      <div className='card-content'>
+        <div className='img-container'>
+          <img className='img-chuck' alt='chuck-logo' src='https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png'/>
+        </div>
+        <div className='joke-container'>
+          <Typography variant="body2" style={{minHeight: '50px'}}>
             {jokes}
           </Typography>
-        </CardContent>
-      </Card>
-      <Button onClick={joke} variant="contained" style={{marginTop: "10px"}}>{jokes ? 'Tell me another one' : 'Tell me a joke, Chuck'}</Button>
-      <div className='buttons-container' style={{marginTop: '10px'}}>
-      <Badge badgeContent={haha} color="success">
-        <Button onClick={()=>vote('like')} color='secondary' variant="contained">Haha!</Button>
-      </Badge>
-      <Badge badgeContent={duuh} color="warning">
-        <Button onClick={vote} variant="outlined">Duuh!</Button>
-      </Badge>
-        
+        </div>
+      </div>
+      <div className='buttons-container'>
+        <Button size='large' color='success' onClick={joke} variant="contained">{jokes===descripcionInicial ? 'Tell me a joke' : 'Another one'}</Button>
+        <div className='votes-container'>
+          <Badge badgeContent={haha} color="primary">
+            <Button onClick={()=>vote('like')} color='secondary' variant="contained">Haha!</Button>
+          </Badge>
+          <Badge badgeContent={duuh} color="warning">
+            <Button onClick={vote} color='error' variant="outlined">Duuh!</Button>
+          </Badge>
+        </div>
       </div>
     </div>
   );
